b9429621471ca3edb24af57c60f2e699a73691d9
AlekUp24 <alekp24@gmail.com>
2024-07-26 14:04:21 +0200

    new way of displaying admin fields

--- akhbota/blog/admin.py
+++ akhbota/blog/admin.py
@@ -1,5 +1,17 @@
+from django import forms
 from django.contrib import admin
 from .models import Blog_Post
-# Register your models here.
 
-admin.site.register(Blog_Post)
+# Create a custom form
+class BlogPostForm(forms.ModelForm):
+    class Meta:
+        model = Blog_Post
+        fields = '__all__'
+        widgets = {
+            'article': forms.Textarea(attrs={'rows': 10, 'cols': 80}),  # Adjust size as needed
+        }
+
+# Register the form with the model admin
+@admin.register(Blog_Post)
+class BlogPostAdmin(admin.ModelAdmin):
+    form = BlogPostForm
