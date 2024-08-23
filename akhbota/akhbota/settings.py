edc83ceb3e3e8a743669b31ef730f6c78d6322a6
AlekUp24 <alekp24@gmail.com>
2024-08-23 13:25:00 +0200

    sa

--- akhbota/akhbota/settings.py
+++ akhbota/akhbota/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
+STATICFILES_DIRS = ['static/']
 #STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # whitenoise static files
@@ -128,7 +128,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 MEDIA_URL = 'media/'
-MEDIA_ROOT = os.path.join(BASE_DIR /'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
