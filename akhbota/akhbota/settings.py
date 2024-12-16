3fa2ba2b006a7c8991c39b6b42d169688e4ca027
AlekUp24 <alekp24@gmail.com>
2024-12-16 20:23:20 +0100

    host set up

--- akhbota/akhbota/settings.py
+++ akhbota/akhbota/settings.py
@@ -18,16 +18,14 @@ SECRET_KEY = 'django-insecure-lelp*qtj0t72!*997ed#48hr_o8=ld%os4ry^q$76_rdh_p8h(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['akhbotawebapp-production.up.railway.app', 
-                'www.akhbotawebapp-production.up.railway.app' , 
-                '127.0.0.1', 
-                'www.akhbota.eu' ,
-                'akhbota.eu']
-                
-CSRF_TRUSTED_ORIGINS = ['https://akhbota.eu', 'https://www.akhbota.eu']
+ALLOWED_HOSTS = ['https://akhbota.eu',
+                'https://akhbotawebapp-production.up.railway.app', 
+                'akhbotawebapp-production.up.railway.app']
 
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-SECURE_SSL_REDIRECT = True
+CSRF_TRUSTED_ORIGINS = ['https://akhbota.eu','https://akhbotawebapp-production.up.railway.app']
+
+#SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+#SECURE_SSL_REDIRECT = True
 
 # Application definition
 
